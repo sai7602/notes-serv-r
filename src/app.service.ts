62a25d9a9542430bb89541f8b2b6715b8ceccef6
OleksandrS <sai7602@ukr.net>
2022-09-29 20:25:58 +0300

    add log

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `POST /notes | DELETE /notes/:id | PATCH /notes/:id | GET /notes/:id |  GET /notes |  GET /notes/stats`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,6 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/');
   await app.listen(3000);
+  console.log('server started on http://localhost:3000');
 }
 bootstrap();
